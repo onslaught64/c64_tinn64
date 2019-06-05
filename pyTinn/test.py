0d6419ef36b01a240ac591b97266a83fc88c3dc6
ZIG of DEFAME <joe@pixolut.com>
2019-06-05 23:22:51 +1000

    wip on training

--- pyTinn/test.py
+++ pyTinn/test.py
@@ -11,7 +11,7 @@ class Data:
 		self.read_data(path, nips, nops)
 
 	def __repr__(self):
-		return f'{len(self)} rows with {len(self.in_[0])} inputs and {len(self.tg[0])} outputs.'
+		return str(len(self)) + " rows with " + str(len(self.in_[0])) + " inputs and " + str(len(self.tg[0])) + " outputs."
 
 	def read_data(self, path, nips, nops):
 		self.in_, self.tg = [], []
@@ -37,18 +37,21 @@ def do_output(o, value:float):
 
 
 nips = 256
-nhid = 28
+nhid = 32
 nops = 10
 rate = 1.0
 anneal = 0.998
-data = Data('semeion.data', nips, nops)
+print("loading training data")
+data = Data('../data/training.data', nips, nops)
+print("model init")
 t = tinn.Tinn(nips, nhid, nops)
 for _ in range(1):
 	data.shuffle()
 	error = 0
+	print("training pass...")
 	for in_, tg in zip(data.in_, data.tg):
 		error += tinn.xttrain(t, in_, tg, rate)
-	print(f'error {error/len(data)} :: learning rate {rate}')
+	print("error " + str(error/len(data)) + " :: learning rate " + str(rate))
 	rate *= anneal
 
 in_ = data.in_[0]
@@ -57,14 +60,7 @@ pd = tinn.xtpredict(t, in_)
 print(' '.join(map(str, tg)))
 print(' '.join(map(str, pd)))
 
-#todo: find the biggest and smallest numbers in the TINN export file!
-rows = 0
-biggest = 0.0
-smallest = 0.0
-longest = 0
-longest_value = ""
-numbers = list()
-
+# export KickAss fixed point neurons and biases 
 output = open("data.asm","w")
 print()
 print()
@@ -83,32 +79,3 @@ for i in t.x2:
 		do_output(output, j)
 
 output.close()
-
-# for line in f:
-# 	rows = rows + 1
-# 	try:
-# 		tmp = float(line.strip())
-# 		if tmp > biggest:
-# 			biggest = tmp
-# 		if tmp < smallest:
-# 			smallest = tmp
-# 		tmpsz = len(str(tmp))
-# 		if tmpsz > longest:
-# 			longest = tmpsz
-# 			longest_value = str(tmp)
-# 		fp = FixedPointNumber(tmp)
-# 		print(" " + str(fp) + " -> " + str(tmp))
-# 		print()
-# 	except ValueError:
-# 		print(str(rows) + " is not a float")
-
-# print(str(rows) + " rows...")
-# print(str(biggest) + " biggest...")
-# print(str(smallest) + " smallest...")
-# print(longest_value + " is " + str(longest))
-
-
-
-
-
-
