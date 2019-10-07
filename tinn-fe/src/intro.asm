1af829474e1b33b6487dde0c47e2085b93b1a6ba
ZIG of DEFAME <joe@pixolut.com>
2019-10-07 22:41:49 +1100

    working vsp

--- tinn-fe/src/intro.asm
+++ tinn-fe/src/intro.asm
@@ -1,8 +1,13 @@
 .import source "lib.asm"
 .import source "const.asm"
 
-.label raster_line_1 = $2f
+.label raster_line_1 = $28
 .label func_spindle = $0c90
+
+.label zp_vsp_fd = $c0
+.label zp_vsp_fe = $c1
+.label zp_vsp_ff = $c2
+
 /*
 Virtual Bitmap Addresses:
 Advanced Art Studio
@@ -68,6 +73,10 @@ func_init:
     sta $d011
     lda #%00111011 //$1b - border is open (with bitmap mode set)
     sta $d011
+    lda #$00
+    sta zp_vsp_fd
+    sta zp_vsp_fe
+    sta zp_vsp_ff
     rts
 
 func_music_enable:
@@ -82,16 +91,11 @@ func_play_music:
     bne !+
     rts
 !:
-    inc $d020
     jsr $e003
-    inc $d020
     jsr $e006
-    inc $d020
     jsr $e006
-    inc $d020
     jsr $e006
     lda #$00
-    sta $d020
     rts
 
 var_music_enable:
@@ -179,108 +183,142 @@ func_frame_wait:
     bne !wait-
     rts
 
-var_initial_yscroll:
-.byte $00
-
-var_char_lines_to_crunch:
-.byte $00
-
 irq1:
 	:startInterrupt()
 	:doubleIRQ(raster_line_1)
-    lda initial_yscroll
-    ora #$50
+
+    // and #%10111111 //;disable invalid gfx mode
+    // sta $d011
+    // lda #$18
+    // sta $d016
+
+    lda #$2f
+waitForRaster:
+    cmp $d012
+    bne waitForRaster
+    bit $d011
+    bmi waitForRaster
+
+    nop
+    nop
+
+    lda #$00		// Prep for VSP
     sta $d011
-    lda #$18 //;use the invalid textmode to "cover up" the linecrunch bug area
-    sta $d016
 
-    lda #$04
-    sta $d020
+    // delay a bit for correct raster pos
+    bit * 		// 4
+    nop 			// 2
+    bit $04  		// 3
+    
+    lda zp_vsp_fe  		// 3
+    sta $d016  		// 4
 
-    lda $d012
-    cmp $d012 //;last cycle of CMP reads data from $d012
-    beq !+ //;add extra cycle if still the same line
-!:
-    ldx #9
-!: 
-    dex
-    bne !-
-    //;this is the part that actually performs the linecrunch
+    // delay!	
+    lda zp_vsp_ff	
+    lsr
+    bcs !next+	// waist 1 cycle for odd number offsets
+!next:
+    tax
+    lda #$60		// self modifying rts to the correct position in waitnops sub routine
+    sta waitnops,x
+    jsr waitnops
+    lda #$ea		// self modifying nop to the correct position in waitnops sub routine
+    sta waitnops,x
+    
+    lda #%01111000
+    sta $d011		// VSP that shit
+    inc zp_vsp_fd
+    ldx zp_vsp_fd
+    lda ffTab,x
+    sta zp_vsp_ff
+    lda feTab,x
+    sta zp_vsp_fe
+
     clc
-    lda var_initial_yscroll
-    ldx var_char_lines_to_crunch
-!crunchloop:
-    adc #%00000001
-    and #%00000111
-    ora #%00110010
-    sta $d011
-    ldy #8 //;notice that there is lots of time left in the rasterline for more effects!
+    lda $d012
+    adc #$08
 !:
-    dey
+    cmp $d012
     bne !-
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    dex
-    bpl !crunchloop-
-    and #%10111111 //;disable invalid gfx mode
+
+    lda $d011
+    and #%10111111
     sta $d011
-    lda #$18
-    sta $d016
-
-
-
-
-
-    lda #$01
-    sta $d020
-
-    lda #$00
-    sta $d020
 
     lda #$ff
     sta REG_SPRITE_ENABLE
 
     jsr func_play_music
+    // lda $d011
+    // ora #%00111000 //$1b - border is open (with bitmap mode set)
+    // sta $d011
 
-    inc sin_index
-    ldy sin_index
-    ldx sintbl,y
-    txa
-    lsr
-    lsr
-    lsr
-    sta char_lines_to_crunch
-    txa
-    and #7
-    eor #7  //;linecrunch scrolls up, but increasing the yscroll scrolls down
-            //;therefore we must flip the yscroll to make it match
-    sta initial_yscroll
-    clc
-    adc #$30-5 //;before linecrunching takes 3 rasterlines
-    sta $d012
+
+
+
+    // and #%10111111 //;disable invalid gfx mode
+    // sta $d011
+    // lda #$18
+    // sta $d016
 
     :mov #<irq1: $fffe
     :mov #>irq1: $ffff
-	//:mov #raster_line_1:$d012
+	:mov #raster_line_1:$d012
 	:mov #$ff: $d019
 	:endInterrupt()
 
+
+
+waitnops:
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		rts
+
+
+
+
 //;precalculated sine-table
 .align $100
-
-sin_index:
-    .byte $00
-
-char_lines_to_crunch:
-    .byte $00
-
-initial_yscroll:
-    .byte $00
-
-sintbl:      
-    .byte 100,102,105,107,110,112,115,117,120,122,124,127,129,131,134,136,138,141,143,145,147,149,151,153,156,158,160,162,163,165,167,169,171,172,174,176,177,179,180,182,183,184,186,187,188,189,190,191,192,193,194,195,196,196,197,198,198,199,199,199,200,200,200,200,200,200,200,200,200,199,199,199,198,198,197,196,196,195,194,193,192,191,190,189,188,187,186,184,183,182,180,179,177,176,174,172,171,169,167,165,163,162,160,158,156,153,151,149,147,145,143,141,138,136,134,131,129,127,124,122,120,117,115,112,110,107,105,102,100,98,95,93,90,88,85,83,80,78,76,73,71,69,66,64,62,59,57,55,53,51,49,47,44,42,40,38,37,35,33,31,29,28,26,24,23,21,20,18,17,16,14,13,12,11,10,9,8,7,6,5,4,4,3,2,2,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,2,2,3,4,4,5,6,7,8,9,10,11,12,13,14,16,17,18,20,21,23,24,26,28,29,31,33,35,37,38,40,42,44,47,49,51,53,55,57,59,62,64,66,69,71,73,76,78,80,83,85,88,90,93,95,98
+feTab:
+	.fill 256, mod((159 + sin(i/256*3.141592654*2.0)*159),8) | %00010000
+ffTab:
+	.fill 256, mod((159 + sin(i/256*3.141592654*2.0)*159)/8,40)
 
